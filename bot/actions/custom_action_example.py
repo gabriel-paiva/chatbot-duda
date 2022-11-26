799f6b813d2290d0709085c3a5c56be7fe07e01b
gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
2022-11-26 15:24:50 -0300

    Adicionar todos os documentos

--- bot/actions/custom_action_example.py
+++ bot/actions/custom_action_example.py
@@ -29,6 +29,8 @@ class ActionExemplo(Action):
 
         return []
 
+# Documentos:
+
 class ActionEnviarDocEstatuto(Action):
     def name(self):
         return "action_enviar_doc_estatuto"
@@ -45,6 +47,110 @@ class ActionEnviarDocEstatuto(Action):
             dispatcher.utter_message("Mas você pode me perguntar sua dúvida" +
                                      " específica que eu farei o máximo para" +
                                      " te responder!")
+
+class ActionEnviarManualDoCalouro(Action):
+    def name(self):
+        return "action_enviar_doc_manual_do_calouro"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("Aqui está o documento do Manual do Calouro")
+
+        try:
+            bot = telegram.Bot(token='TELEGRAM_TOKEN')
+            url = 'https://ucb.catolica.edu.br/portal/wp-content/uploads/2022/03/GUIA-DO-ESTUDANTE02.pdf'
+            bot.sendDocument(chat_id=tracker.sender_id, document=url)
+        except Exception:
+            dispatcher.utter_message("Desculpe, não consegui acessar o documento :/")
+            dispatcher.utter_message("Mas você pode me perguntar sua dúvida" +
+                                     " específica que eu farei o máximo para" +
+                                     " te responder!")
+
+class ActionEnviarCodigoDeConduta(Action):
+    def name(self):
+        return "action_enviar_doc_codigo_de_conduta"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("Aqui está o documento do Código de Conduta")
+
+        try:
+            bot = telegram.Bot(token='TELEGRAM_TOKEN')
+            url = 'https://ucb.catolica.edu.br/portal/wp-content/uploads/2020/12/CODIGO_CONDUTA_ETICA_UBEC.pdf'
+            bot.sendDocument(chat_id=tracker.sender_id, document=url)
+        except Exception:
+            dispatcher.utter_message("Desculpe, não consegui acessar o documento :/")
+            dispatcher.utter_message("Mas você pode me perguntar sua dúvida" +
+                                     " específica que eu farei o máximo para" +
+                                     " te responder!")
+
+class ActionEnviarCartaDePrincipios(Action):
+    def name(self):
+        return "action_enviar_doc_carta_de_principios"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("Aqui está o documento da Carta de Principios da UBEC")
+
+        try:
+            bot = telegram.Bot(token='TELEGRAM_TOKEN')
+            url = 'https://ucb.catolica.edu.br/portal/wp-content/uploads/2019/01/CartadePrincipios.pdf'
+            bot.sendDocument(chat_id=tracker.sender_id, document=url)
+        except Exception:
+            dispatcher.utter_message("Desculpe, não consegui acessar o documento :/")
+            dispatcher.utter_message("Mas você pode me perguntar sua dúvida" +
+                                     " específica que eu farei o máximo para" +
+                                     " te responder!")
+
+class ActionEnviarRegulamentoGeral(Action):
+    def name(self):
+        return "action_enviar_doc_regulamento_geral"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("Aqui está o documento de Regulamento Geral da Graduação")
+
+        try:
+            bot = telegram.Bot(token='TELEGRAM_TOKEN')
+            url = 'https://ucb.catolica.edu.br/portal/wp-content/uploads/2022/02/Regulamento_Geral_da_Graduacao.pdf'
+            bot.sendDocument(chat_id=tracker.sender_id, document=url)
+        except Exception:
+            dispatcher.utter_message("Desculpe, não consegui acessar o documento :/")
+            dispatcher.utter_message("Mas você pode me perguntar sua dúvida" +
+                                     " específica que eu farei o máximo para" +
+                                     " te responder!")
+
+class ActionEnviarNormaseProcedimentos(Action):
+    def name(self):
+        return "action_enviar_doc_normas_procedimentos"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("Aqui está o documento de Normas e Procedimentos Acadêmicos da UCB")
+
+        try:
+            bot = telegram.Bot(token='TELEGRAM_TOKEN')
+            url = 'https://ucb.catolica.edu.br/portal/wp-content/uploads/2022/02/Normas-e-Procedimentos-Academicos.pdf'
+            bot.sendDocument(chat_id=tracker.sender_id, document=url)
+        except Exception:
+            dispatcher.utter_message("Desculpe, não consegui acessar o documento :/")
+            dispatcher.utter_message("Mas você pode me perguntar sua dúvida" +
+                                     " específica que eu farei o máximo para" +
+                                     " te responder!")
+
+class ActionEnviarMIV(Action):
+    def name(self):
+        return "action_enviar_doc_miv"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message("Aqui está o documento do Manual de Identidade Visual da UCB")
+
+        try:
+            bot = telegram.Bot(token='TELEGRAM_TOKEN')
+            url = 'https://ucb.catolica.edu.br/portal/wp-content/uploads/2022/02/Manual-de-Identidade-CATOLICA-UCB-1.pdf'
+            bot.sendDocument(chat_id=tracker.sender_id, document=url)
+        except Exception:
+            dispatcher.utter_message("Desculpe, não consegui acessar o documento :/")
+            dispatcher.utter_message("Mas você pode me perguntar sua dúvida" +
+                                     " específica que eu farei o máximo para" +
+                                     " te responder!")
+
+# Localizações:
 
 class ActionEnviarLocalizacaoBlocoABCD(Action):
     def name(self):
